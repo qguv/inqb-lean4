912d127ade4d9429121562a9799a622fadbc851c
Quint Guvernator <quint@guvernator.net>
2024-12-06 21:09:05 +0100

    template for first book proof

---
+++ Logic/Inquisitive/bookproofs.lean
@@ -0,0 +1,9 @@
+import Logic.Inquisitive.types
+import Logic.Inquisitive.ops
+import Logic.Inquisitive.projections
+
+namespace Inquisitive
+
+-- a.k.a. book exercise 3.6, homework 1 exercise 3
+theorem fact_3_14 (p : Proposition W) : p = Proposition.meet p.info p.decisionSet := by
+  sorry
