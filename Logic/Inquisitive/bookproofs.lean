85acddc08c49a7f5a56e71857355cf9df98f4c7c
Quint Guvernator <quint@guvernator.net>
2024-12-07 13:23:36 +0100

    WIP

--- Logic/Inquisitive/bookproofs.lean
+++ Logic/Inquisitive/bookproofs.lean
@@ -1,9 +1,15 @@
 import Logic.Inquisitive.types
 import Logic.Inquisitive.ops
 import Logic.Inquisitive.projections
+import Logic.Inquisitive.lemmas
 
 namespace Inquisitive
 
 -- a.k.a. book exercise 3.6, homework 1 exercise 3
 theorem fact_3_14 (p : Proposition W) : p = Proposition.meet p.info p.decisionSet := by
+  let info := p.info
+  have h1 := lemmas.info_is_double_absolutePseudoComplement p
+  have h2 := Eq.mp (a := p.info) (b := p.absolutePseudoComplement.absolutePseudoComplement) h1 p.info
+  rw [h] at p
+  have p := p.info.containsEmpty
   sorry
---
+++ Logic/Inquisitive/lemmas.lean
@@ -0,0 +1,9 @@
+import Logic.Inquisitive.types
+import Logic.Inquisitive.ops
+import Logic.Inquisitive.projections
+
+namespace Inquisitive.lemmas
+
+-- a.k.a. book exercise 3.6, homework 1 exercise 3
+theorem info_is_double_absolutePseudoComplement (p : Proposition W) : p.info = p.absolutePseudoComplement.absolutePseudoComplement := by
+  sorry
