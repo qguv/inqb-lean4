2cc622a2936d2ba5824c5a47c07d6ed6993bed3b
Quint Guvernator <quint@guvernator.net>
2024-12-07 13:23:43 +0100

    WIP

---
+++ Logic/Inquisitive/syntax.lean
@@ -0,0 +1,25 @@
+import Lean
+
+import Logic.Inquisitive.types
+import Logic.Inquisitive.ops
+import Logic.Inquisitive.projections
+
+namespace Inquisitive
+
+macro p:ident "*" : term => do
+  `(Proposition.absolutePseudoComplement $p)
+
+macro p:ident " ∩ᵢ " q:ident : term => do
+  `(Proposition.meet $p $q)
+
+macro p:ident " ⋃ᵢ " q:ident : term => do
+  `(Proposition.join $p $q)
+
+macro "?" p:ident : term => do
+  `(Proposition.decisionSet $p)
+
+macro "!" p:ident : term => do
+  `(Proposition.info $p)
+
+macro "⁉" p:ident : term => do
+  `(Proposition.info (Proposition.decisionSet $p))
