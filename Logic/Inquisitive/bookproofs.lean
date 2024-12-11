fcdf8c977c7a186ce30b36be6f31671975f03082
Quint Guvernator <quint@guvernator.net>
2024-12-11 14:20:52 +0100

    WIP again

--- Logic/Inquisitive/bookproofs.lean
+++ Logic/Inquisitive/bookproofs.lean
@@ -18,28 +18,44 @@ theorem fact_3_14 (p : Proposition W) : p = p.info.meet p.decisionSet := by
   have h1 := lemmas.info_is_double_absolutePseudoComplement p
   apply lemmas.eq_of_truthSet_eq
   unfold Proposition.meet
-  simp
+  simp only
+  apply lemmas.eq_of_truthSet_eq at h1
   rw [h1]
   unfold Proposition.absolutePseudoComplement
-  simp
+  simp only
   unfold Proposition.decisionSet
-  simp
+  simp only
   unfold Proposition.absolutePseudoComplement
-  simp
+  simp only
+  --simp
   -- ext turns equalities into bi-implications
   ext x
   constructor
-  · intro x_in
+  case a.h.mp =>
+    intro s
+    simp only [Set.mem_inter_iff, Set.mem_powerset_iff, Set.mem_union]
+    constructor
+    case right => exact Or.inl s
+    case left =>
+      intro t
+      intro h
+      rw [Set.mem_compl_iff]
+      --rw [Set.not_mem_of_not_mem_sUnion]
+      --apply Not.intro
+      intro u
+      --rw [Set.mem_def]
+      sorry
+  case a.h.mpr =>
+    intro h2
+    cases h2.right with
+    | inl h3 => exact h3
+    | inr h3 =>
+      sorry
     -- simp only is like rw but applies any number of times and in any order
     --simp?
     --show_term simp
     --consider always converting simp to simp only
     --simp only [Set.mem_inter_iff, Set.mem_powerset_iff, Set.mem_union]
-    sorry
-
-  ·
-
-    sorry
 
 
   -- rcases p with ⟨X, hX, hX'⟩
