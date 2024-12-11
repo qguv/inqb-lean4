698ed87a9ccbe0dc15554b732937de858ecf544a
Quint Guvernator <quint@guvernator.net>
2024-12-11 14:20:46 +0100

    relocate lemma

--- Logic/Inquisitive/bookproofs.lean
+++ Logic/Inquisitive/bookproofs.lean
@@ -11,15 +11,12 @@ variable {W : Type}
 
 #print lemmas.info_is_double_absolutePseudoComplement
 
--- maybe make this an iff and tell the `ext` tactic to use it?
-lemma Proposition.eq_of_truthSet_eq {p q : Proposition W} : p.truthSet = q.truthSet → p = q := sorry
-
 -- a.k.a. book exercise 3.6, homework 1 exercise 3
 theorem fact_3_14 (p : Proposition W) : p = p.info.meet p.decisionSet := by
   --let not_info := p.info.truthSetᶜ
   --let px := p
   have h1 := lemmas.info_is_double_absolutePseudoComplement p
-  apply Proposition.eq_of_truthSet_eq
+  apply lemmas.eq_of_truthSet_eq
   unfold Proposition.meet
   simp
   rw [h1]
--- Logic/Inquisitive/lemmas.lean
+++ Logic/Inquisitive/lemmas.lean
@@ -7,3 +7,6 @@ namespace Inquisitive.lemmas
 -- a.k.a. book exercise 3.6, homework 1 exercise 3
 theorem info_is_double_absolutePseudoComplement (p : Proposition W) : p.info.truthSet = p.absolutePseudoComplement.absolutePseudoComplement.truthSet := by
   sorry
+
+-- TODO maybe make this an iff and tell the `ext` tactic to use it?
+theorem eq_of_truthSet_eq {p q : Proposition W} : p.truthSet = q.truthSet → p = q := sorry
