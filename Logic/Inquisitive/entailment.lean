7cbe8ff616f19872159fe4e012ac00a405619bfb
Quint Guvernator <quint@guvernator.net>
2024-12-18 01:53:49 +0100

    define tautology

--- Logic/Inquisitive/entailment.lean
+++ Logic/Inquisitive/entailment.lean
@@ -12,4 +12,5 @@ def Proposition.info := ⋃₀ p.truthSet
 def Proposition.trueIn (w : W) := w ∈ p.info
 def Proposition.isInformative := p.info ≠ Set.univ
 def Proposition.isInquisitive := p.info ∉ p.truthSet
+def Proposition.isTautology := Set.univ ∈ p.truthSet
 def Proposition.alt := { s ∈ p.truthSet | ¬(∃t ∈ p.truthSet, s ⊂ t) }
